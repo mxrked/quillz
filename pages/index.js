ebc360b650268349191155e0ad67d4a2778ad28c
Parker <pp101socials@gmail.com>
2023-04-03 12:33:22 -0400

    Created file

--- pages/index.js
+++ pages/index.js
@@ -7,15 +7,20 @@ import { useRouter } from "next/router";
 // Data/Functions/Images Imports
 
 // Component Imports
+import { PageHead } from "@/assets/components/global/PageHead";
 
 // Style Imports
 import "../assets/styles/modules/Index/Index.module.css";
 
 export default function Home() {
-
   const router = useRouter();
 
   return (
-    ""
-  )
+    <div id="page" className="page index-page overrides_INDEX">
+      {/* Global Components */}
+      <PageHead />
+
+      <main id="pageMain" className="page-main"></main>
+    </div>
+  );
 }
