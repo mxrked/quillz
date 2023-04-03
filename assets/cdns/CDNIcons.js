d266e26820a00785a24a3207de368f6b1e6cf0f5
Parker <pp101socials@gmail.com>
2023-04-03 11:58:35 -0400

    Added tab icons

--- assets/cdns/CDNIcons.js
+++ assets/cdns/CDNIcons.js
@@ -6,4 +6,9 @@
 
 import { CDNIconReturn } from "./CDNReturns";
 
-export {};
+const ATI = CDNIconReturn("tab-icons", "apple-touch-icon", "png");
+const FAVICON = CDNIconReturn("tab-icons", "favicon", "ico");
+const F32 = CDNIconReturn("tab-icons", "favicon-32x32", "png");
+const F16 = CDNIconReturn("tab-icons", "favicon-16x16", "png");
+
+export { ATI, FAVICON, F16, F32 };
