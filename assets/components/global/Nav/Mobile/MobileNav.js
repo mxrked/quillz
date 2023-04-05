eaa0a5e678bb43bdb7445432d3853172bb112b9d
Parker <pp101socials@gmail.com>
2023-04-05 11:25:22 -0400

    Created file

---
+++ assets/components/global/Nav/Mobile/MobileNav.js
@@ -0,0 +1,159 @@
+/**
+ *
+ *  This is the mobile nav
+ *
+ */
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { FaCaretDown, FaSearch, FaShoppingCart } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
+import { useAnimation, motion } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+
+import TriggerInViewMotion from "@/assets/functions/dom/triggers/TriggerInViewMotion";
+import DeclareStorageVariable from "@/assets/functions/data/storage/DeclareStorageVariable";
+
+import { FADE_IN } from "@/assets/anims/FADE_IN";
+
+import styles from "../../../../styles/modules/Nav/Nav.module.css";
+
+export const MobileNav = () => {
+  const router = useRouter();
+
+  //! Framer Motion Detect InView
+  const CONTROLS = useAnimation();
+  const [REF, INVIEW] = useInView();
+  useEffect(() => {
+    TriggerInViewMotion(CONTROLS, INVIEW);
+  }, [CONTROLS, INVIEW]);
+
+  return (
+    <nav id="mobileNav" className={`${styles.mobile_nav} mobile-nav`}>
+      <motion.div
+        className={`${styles.motion} fm-motion fade-in full-second`}
+        ref={REF}
+        initial="hidden"
+        animate={CONTROLS}
+        variants={FADE_IN}
+      >
+        <div className={`${styles.mobile_nav_inner}`}>
+          <div className={`${styles.mobile_nav_inner_box} container-fluid`}>
+            <div className={`${styles.mobile_nav_inner_row} row`}>
+              <div
+                className={`${styles.mobile_nav_inner_side} ${styles.mobile_nav_L} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
+              >
+                <div className={`${styles.mobile_nav_inner_side_cnt}`}>
+                  <a href="/" className={`${styles.logo} half-second`}>
+                    <div>
+                      <span className={`${styles.top_text} half-second`}>
+                        Quillz
+                      </span>
+                      <span className={`${styles.bottom_text} half-second`}>
+                        All things hedgie.
+                      </span>
+                    </div>
+                  </a>
+                </div>
+              </div>
+              <div
+                className={`${styles.mobile_nav_inner_side} ${styles.mobile_nav_R} col-lg-6 col-md-6 col-sm-6 col-xs-6`}
+              >
+                <div className={`${styles.mobile_nav_inner_side_cnt}`}>
+                  <div className={`${styles.side_links}`}>
+                    <div className={`${styles.top}`}>
+                      <ul className={`${styles.side_links}`}>
+                        <li className={`${styles.profile_link} profile-link`}>
+                          <a href="/profile" className="half-second">
+                            <span>My Profile</span>{" "}
+                            <MdAccountCircle className={`${styles.icon}`} />
+                          </a>
+                        </li>
+                        <li
+                          className={`${styles.login_register_link} login-register-link`}
+                        >
+                          <a href="/login_register" className="half-second">
+                            <span>Login/Register</span>{" "}
+                          </a>
+                        </li>
+                      </ul>
+                    </div>
+
+                    <div className={`${styles.bottom}`}>
+                      <div className={`${styles.cart_link}`}>
+                        <a href="/cart" className="half-second">
+                          <FaShoppingCart className={`${styles.icon}`} />
+
+                          <div>
+                            <span className="cart-counter">0</span>
+                          </div>
+                        </a>
+                      </div>
+                      <button
+                        id="searchToggler"
+                        className={`${styles.search_toggler} search-toggler half-second`}
+                      >
+                        <FaSearch className={`${styles.icon}`} />
+                      </button>
+                    </div>
+                  </div>
+
+                  <button
+                    id="mobileNavToggler"
+                    className={`${styles.mobile_nav_toggler}`}
+                    onClick={() => {
+                      DeclareStorageVariable(
+                        "session",
+                        "Mobile Nav Opened",
+                        true
+                      );
+
+                      document.body.style.overflowY = "hidden";
+                      document.body.style.pointerEvents = "none";
+
+                      const MENU = document.getElementById("mobileNavMenu");
+                      const DARKEN = document.getElementById(
+                        "mobileNavMenuDarken"
+                      );
+                      const MAIN = document.getElementById("mobileNavMenuMain");
+
+                      MENU.style.display = "block";
+
+                      setTimeout(() => {
+                        DARKEN.style.opacity = 1;
+                        DARKEN.style.visibility = "visible";
+                      }, 500);
+
+                      setTimeout(() => {
+                        MAIN.style.width = "100%";
+                      }, 900);
+
+                      setTimeout(() => {
+                        document
+                          .querySelectorAll(".mobile-nav-menu-cnt")
+                          .forEach((cnt) => {
+                            cnt.style.opacity = 1;
+                            cnt.style.visibility = "visible";
+                          });
+                      }, 1800);
+
+                      setTimeout(() => {
+                        MENU.style.overflowY = "auto";
+                        MENU.style.pointerEvents = "auto";
+                      }, 2400);
+                    }}
+                  >
+                    <span className="half-second" />
+                    <span className="half-second" />
+                    <span className="half-second" />
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </nav>
+  );
+};
---
+++ assets/components/global/Nav/Mobile/MobileNavMenu.js
@@ -0,0 +1,191 @@
+/**
+ *
+ *  This is the mobile nav menu
+ *
+ */
+
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { FaCaretDown, FaSearch, FaShoppingCart, FaTimes } from "react-icons/fa";
+import { MdAccountCircle } from "react-icons/md";
+
+import CloseMobileNav from "@/assets/functions/dom/closers/CloseMobileNav";
+
+import styles from "../../../../styles/modules/Nav/Nav.module.css";
+
+export const MobileNavMenu = () => {
+  return (
+    <div id="mobileNavMenu" className={`${styles.mobile_nav_menu}`}>
+      <div
+        className={`${styles.darken} full-second`}
+        id="mobileNavMenuDarken"
+      />
+
+      <div
+        className={`${styles.mobile_nav_menu_main} full-second`}
+        id="mobileNavMenuMain"
+      >
+        <div className={`${styles.main_top} mobile-nav-menu-cnt half-second`}>
+          <button
+            id="mobileNavCloser"
+            className={`${styles.mobile_nav_closer} half-second`}
+            onClick={() => {
+              CloseMobileNav();
+            }}
+          >
+            <FaTimes className={`${styles.icon}`} />
+          </button>
+        </div>
+
+        <div className={`${styles.main_links} mobile-nav-menu-cnt half-second`}>
+          <a href="/" className={`${styles.link} nav-link half-second`}>
+            <span className="half-second">Home</span>
+          </a>
+
+          <div className={`${styles.sub_links_toggler} half-second`}>
+            <input
+              type="checkbox"
+              id="mobileNavMenuProductsCB"
+              onChange={(e) => {
+                const LINKS = document.getElementById(
+                  "mobileNavMenuProductsLinks"
+                );
+
+                if (e.currentTarget.checked) {
+                  LINKS.style.height = "180px";
+                  document.querySelectorAll(
+                    ".sub-links-toggler-icon"
+                  )[0].style.transform = "rotate(180deg)";
+                } else {
+                  LINKS.style.height = 0;
+                  document.querySelectorAll(
+                    ".sub-links-toggler-icon"
+                  )[0].style.transform = "rotate(0deg)";
+                }
+              }}
+            />
+
+            <FaCaretDown
+              className={`${styles.icon} sub-links-toggler-icon half-second`}
+            />
+
+            <span>Products</span>
+          </div>
+
+          <ul id="mobileNavMenuProductsLinks" className="half-second">
+            <li className="half-second">
+              <a href="/products#food" className="nav-link half-second">
+                <span>Food</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/products#toys" className="nav-link half-second">
+                <span>Toys</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/products#housing" className="nav-link half-second">
+                <span>Housing</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/products" className="nav-link half-second">
+                <span>View All</span>
+              </a>
+            </li>
+          </ul>
+
+          <div className={`${styles.sub_links_toggler} half-second`}>
+            <input
+              type="checkbox"
+              id="mobileNavMenuTypesCB"
+              onChange={(e) => {
+                const LINKS = document.getElementById(
+                  "mobileNavMenuTypesLinks"
+                );
+
+                if (e.currentTarget.checked) {
+                  LINKS.style.height = "270px";
+                  document.querySelectorAll(
+                    ".sub-links-toggler-icon"
+                  )[1].style.transform = "rotate(180deg)";
+                } else {
+                  LINKS.style.height = 0;
+                  document.querySelectorAll(
+                    ".sub-links-toggler-icon"
+                  )[1].style.transform = "rotate(0deg)";
+                }
+              }}
+            />
+
+            <FaCaretDown
+              className={`${styles.icon} sub-links-toggler-icon half-second`}
+            />
+
+            <span>Types</span>
+          </div>
+
+          <ul id="mobileNavMenuTypesLinks" className="half-second">
+            <li className="half-second">
+              <a href="/types#atelerix" className="nav-link half-second">
+                <span>Atelerix</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/types#erinaceus" className="nav-link half-second">
+                <span>Erinaceus</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/types#hemiechinus" className="nav-link half-second">
+                <span>Hemiechinus</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/types#mesechinus" className="nav-link half-second">
+                <span>Mesechinus</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/types#paraechinus" className="nav-link half-second">
+                <span>Paraechinus</span>
+              </a>
+            </li>
+            <li className="half-second">
+              <a href="/types" className="nav-link half-second">
+                <span>View All</span>
+              </a>
+            </li>
+          </ul>
+
+          <a
+            href="/info_contact"
+            className={`${styles.link} nav-link half-second`}
+          >
+            <span className="half-second">Info/Contact</span>
+          </a>
+          <a
+            href="/login_register"
+            className={`${styles.link} login-register-link nav-link half-second`}
+          >
+            <span className="half-second">Login/Register</span>
+          </a>
+          <a
+            href="/profile"
+            className={`${styles.link} profile-link nav-link half-second`}
+          >
+            <span className="half-second">My Profile</span>
+          </a>
+          <a href="/cart" className={`${styles.link} nav-link half-second`}>
+            <span className="half-second">My Cart</span>
+
+            <div>
+              <span className="cart-counter">0</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  );
+};
