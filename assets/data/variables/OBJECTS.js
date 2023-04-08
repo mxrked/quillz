98af45a45ed3016470de80e73aece9dad144d776
Parker <pp101socials@gmail.com>
2023-04-08 18:31:02 -0400

    Created objects

--- assets/data/variables/OBJECTS.js
+++ assets/data/variables/OBJECTS.js
@@ -4,22 +4,22 @@ import Hedgehog from "../classes/Hedgehog";
 import IndexTopSlide from "../classes/IndexTopSlide";
 
 const INDEX_TOP_SLIDE_1 = new IndexTopSlide(
-  "#SLIDE_1",
+  "SLIDE_1",
   "Provide for your hedgehog.",
-  "There is so much to learn about hedgehogs and why they are so great.",
+  "Browse our great products that are sure to make your hedgehog happy.",
   "https://raw.githubusercontent.com/mxrked/quillz_CDN/master/bgs/eating/eating-1.webp",
   "/products"
 );
 const INDEX_TOP_SLIDE_2 = new IndexTopSlide(
-  "#SLIDE_2",
-  "We have a love for all things hedgie.",
+  "SLIDE_2",
+  "We love all things hedgie.",
   "There is so much to learn about hedgehogs and why they are so great.",
   "https://raw.githubusercontent.com/mxrked/quillz_CDN/master/bgs/hedgehog-in-grass/hedgehog-in-grass-1.webp",
-  "/info"
+  "/info_contact"
 );
 const INDEX_TOP_SLIDE_3 = new IndexTopSlide(
-  "#SLIDE_3",
-  "Learn the legacy of hedgehogs.",
+  "SLIDE_3",
+  "A legacy of hedgehogs.",
   "There are different types of hedgehog in the world, so why not learn them.",
   "https://raw.githubusercontent.com/mxrked/quillz_CDN/master/bgs/pygmy/pygmy-2.webp",
   "/types"
