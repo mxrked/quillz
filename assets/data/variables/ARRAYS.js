14335089fbd57cdea5ee3e1426d287b8ae908e3b
Parker <pp101socials@gmail.com>
2023-04-12 11:45:19 -0400

    Added arrays

--- assets/data/variables/ARRAYS.js
+++ assets/data/variables/ARRAYS.js
@@ -23,31 +23,41 @@ const TYPES_KWS = [
   "different types",
   "hedgehog types",
   "hedgehogs",
+];
+const ATELERIX_KWS = [
   "atelerix",
-  "erinaceus",
-  "hemiechinus",
-  "mesechinus",
-  "paraechinus",
   "african pygmy",
   "algerian",
   "southern african",
   "somali",
+];
+const ERINACEUS_KWS = [
+  "erinaceus",
   "amur",
   "southern white-breasted",
   "southern white breasted",
   "european",
   "northern white-breasted",
   "northern white breasted",
+];
+const HEMIECHINUS_KWS = [
+  "hemiechinus",
   "egyptian long-eared",
   "egyptian long eared",
   "indian long-eared",
   "indian long eared",
+];
+const MESECHINUS_KWS = [
+  "mesechinus",
   "daurian",
   "hugh's",
   "hughs",
   "small-toothed forest",
   "small toothed forest",
   "gaoligong forest",
+];
+const PARAECHINUS_KWS = [
+  "paraechinus",
   "desert",
   "brandt's",
   "brandts",
@@ -68,6 +78,9 @@ const PRODUCTS_KWS = [
   "hedgehog toys",
   "hedgehog housing",
 ];
+const TOYS_KWS = ["toys", "hedgehog toys"];
+const FOOD_KWS = ["food", "hedgehog food"];
+const HOUSING_KWS = ["housing", "hedgehog housing"];
 const INFO_CONTACT_KWS = [
   "info",
   "contact",
@@ -91,6 +104,20 @@ const INDEX_TOP_SLIDES = [
   INDEX_TOP_SLIDE_3,
 ];
 
-export { INDEX_KWS, INFO_CONTACT_KWS, TYPES_KWS, PRODUCTS_KWS, CART_KWS };
+export {
+  INDEX_KWS,
+  INFO_CONTACT_KWS,
+  TYPES_KWS,
+  ATELERIX_KWS,
+  ERINACEUS_KWS,
+  MESECHINUS_KWS,
+  HEMIECHINUS_KWS,
+  PARAECHINUS_KWS,
+  PRODUCTS_KWS,
+  TOYS_KWS,
+  FOOD_KWS,
+  HOUSING_KWS,
+  CART_KWS,
+};
 export { FADES };
 export { INDEX_TOP_SLIDES };
