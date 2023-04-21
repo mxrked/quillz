f243802517ea27a034b91d93538ba4764a2edb3a
Parker <pp101socials@gmail.com>
2023-04-21 10:58:51 -0400

    Added component and changed routing for logged in user

--- pages/login_register.js
+++ pages/login_register.js
@@ -5,28 +5,27 @@ import { useRouter } from "next/router";
 // Library Imports
 
 // Data/Functions/Images Imports
+import { TriggerExitAnimations } from "@/assets/functions/dom/triggers/TriggerExitAnimations";
 
 // Component Imports
 import { PageHead } from "@/assets/components/global/PageHead";
+import { Login_RegisterMain } from "@/assets/components/pages/Login_Register/Login_RegisterMain";
 
 // Style Imports
 import "../assets/styles/modules/Login_Register/Login_Register.module.css";
-
 export default function Login_Register() {
   const router = useRouter();
+
+  // Exit animations trigger
+  useEffect(() => {
+    TriggerExitAnimations();
+  }, []);
 
   // Checking if user is logged in, takes them back to home
   useEffect(() => {
     // Checking sessionStorage
-    if (sessionStorage.getItem("Logged In")) {
-      if (
-        sessionStorage.getItem("Username") &&
-        sessionStorage.getItem("Encrypted Password")
-      ) {
-        router.push("/");
-      } else {
-        return;
-      }
+    if (sessionStorage.getItem("User Email")) {
+      router.push("/");
     } else {
       return;
     }
@@ -40,7 +39,9 @@ export default function Login_Register() {
       {/* Global Components */}
       <PageHead />
 
-      <main id="pageMain" className="page-main"></main>
+      <main id="pageMain" className="page-main">
+        <Login_RegisterMain />
+      </main>
     </div>
   );
 }
